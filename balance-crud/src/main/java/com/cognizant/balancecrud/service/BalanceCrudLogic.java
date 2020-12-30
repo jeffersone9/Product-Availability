d9e11955c60c03992c2f4af02098acd13507d565
Elijah Jefferson <jeffersone9@gmail.com>
2020-12-30 14:20:47 -0600

    back to jars

--- balance-crud/src/main/java/com/cognizant/balancecrud/service/BalanceCrudLogic.java
+++ balance-crud/src/main/java/com/cognizant/balancecrud/service/BalanceCrudLogic.java
@@ -2,10 +2,12 @@ package com.cognizant.balancecrud.service;
 
 import com.cognizant.balancecrud.exception.BalanceNotFoundException;
 import com.cognizant.balancecrud.model.BalanceDB;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 
 public interface BalanceCrudLogic {
+    @Cacheable("balances")
     List<BalanceDB> findAll();
 
     BalanceDB getBalanceById(long id) throws BalanceNotFoundException;
